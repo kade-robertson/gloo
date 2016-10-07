2970017777b2dae67207c25c74cb379fa2b59a02
kade-robertson <kade.robertson96@gmail.com>
2016-10-07 12:30:03 -0400

    Initial commit

---
+++ gloo.py
@@ -0,0 +1,55 @@
+import sys
+import argparse
+from gloocode import *
+
+allnums = '0123456789.'
+
+def numparse(n):
+    if '.' in n:
+        return float(n)
+    else:
+        return int(n)
+
+def process(program):
+    stack = []
+    pos = 0
+    while pos < len(program):
+        if program[pos] in allnums[:-1]:
+            numstr = program[pos]
+            hasdot = False
+            pos += 1
+            while pos < len(program) and program[pos] in allnums:
+                if program[pos] == '.':
+                    if hasdot: break
+                    else: hasdot = True
+                numstr += program[pos]
+                pos += 1
+            pos -= 1
+            stack.append(numparse(numstr))
+        elif program[pos] == '"':
+            sstr = ''
+            pos += 1
+            while pos < len(program) and program[pos] != '"':
+                sstr += program[pos]
+                pos  += 1
+            stack.append(sstr)
+        else:
+            codepage[hex(ord(program[pos]))](stack)
+        pos += 1
+    print stack
+
+def main():
+    pargs = argparse.ArgumentParser( description = 'Interpreter for the Gloo programming language.' )
+    pargs.add_argument('-f', nargs = '?', help='read Gloo program from a file')
+    args, rest = pargs.parse_known_args()
+    if args.f is not None:
+        with open(args.f) as prog:
+            process(prog.read())
+    else:
+        prog = raw_input("gloo # ")
+        while prog != 'ungloo me!':
+            process(prog)
+            prog = raw_input("gloo # ")
+        
+if __name__ == '__main__':
+    main()
---
+++ gloofunc.py
@@ -0,0 +1,116 @@
+import operator
+
+def add(stk):
+    if len(stk) == 0: return
+    k = stk.pop()
+    if type(k) is list:
+        if all(isinstance(i, (int, long, float)) for i in k):
+            stk.append(sum(k))
+        elif all(isinstance(i, str) for i in k):
+            stk.append(''.join(k))
+        else:
+            stk.append(k)
+    elif type(k) in (int, long, float):
+        if len(stk) == 0:
+            stk.append(k)
+            return
+        j = stk.pop()
+        if type(j) in (int, long, float):
+            stk.append(k + j)
+        else:
+            stk.append(j)
+            stk.append(k)
+    elif type(k) is str:
+        if len(stk) == 0:
+            stk.append(k)
+            return
+        j = stk.pop()
+        if type(j) is str:
+            stk.append(j + k)
+        else:
+            stk.append(j)
+            stk.append(k)
+    else:
+        stk.append(k)
+
+def subt(stk):
+    if len(stk) == 0: return
+    k = stk.pop()
+    if type(k) is list:
+        k = [i for i in k if i]
+        stk.append(k)
+    elif type(k) in (int, long, float):
+        if len(stk) == 0:
+            stk.append(k)
+            return
+        j = stk.pop()
+        if type(j) in (int, long, float):
+            stk.append(j - k)
+        else:
+            stk.append(j)
+            stk.append(k)
+    elif type(k) is str:
+        if len(stk) == 0:
+            stk.append(k)
+            return
+        j = stk.pop()
+        if type(j) is str:
+            stk.append(j.replace(k, ''))
+        else:
+            stk.append(j)
+            stk.append(k)
+    else:
+        stk.append(k)
+
+def mult(stk):
+    if len(stk) == 0: return
+    k = stk.pop()
+    if type(k) is list:
+        if all(isinstance(i, (int, long, float)) for i in k):
+            stk.append(reduce(operator.mul, k, 1))
+        else:
+            stk.append(k)
+    elif type(k) in (int, long, float):
+        if len(stk) == 0:
+            stk.append(k)
+            return
+        j = stk.pop()
+        if type(j) in (int, long, float):
+            stk.append(j * k)
+        elif type(k) in (int, long) and type(j) is str:
+            stk.append(j * k)
+        else:
+            stk.append(j)
+            stk.append(k)
+    elif type(k) is str:
+        if len(stk) == 0:
+            stk.append(k)
+            return
+        j = stk.pop()
+        if type(j) in (int, long):
+            stk.append(j * k)
+        else:
+            stk.append(j)
+            stk.append(k)
+    else:
+        stk.append(k)
+        
+        
+
+def wrap(stk):
+    newl = []
+    while len(stk) > 0:
+        newl = [stk.pop()] + newl
+    stk.append(newl)
+
+def uwrap(stk):
+    if len(stk) == 0: return
+    k = stk.pop()
+    if type(k) is list:
+        for item in k:
+            stk.append(item)
+    else:
+        stk.append(K)
+
+def noop(stk):
+    return
