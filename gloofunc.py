98f415163a83c5a7e654e3c2b4e9920b3cce5c58
kade-robertson <kade.robertson96@gmail.com>
2016-10-08 14:33:26 -0400

    some functions now prompt for input if not enough stack items

--- gloofunc.py
+++ gloofunc.py
@@ -2,7 +2,8 @@ import math
 import operator
 
 def add(stk):
-    if len(stk) == 0: return
+    if len(stk) == 0:
+        stk.append(input())
     k = stk.pop()
     if type(k) is list:
         if all(isinstance(i, (int, long, float)) for i in k):
@@ -12,20 +13,24 @@ def add(stk):
         else:
             stk.append(k)
     elif type(k) in (int, long, float):
+        j = 0
         if len(stk) == 0:
-            stk.append(k)
-            return
-        j = stk.pop()
+            j = input()
+            j,k = k,j
+        else:
+            j = stk.pop()
         if type(j) in (int, long, float):
             stk.append(k + j)
         else:
             stk.append(j)
             stk.append(k)
     elif type(k) is str:
+        j = 0
         if len(stk) == 0:
-            stk.append(k)
-            return
-        j = stk.pop()
+            j = input()
+            j,k = k,j
+        else:
+            j = stk.pop()
         if type(j) is str:
             stk.append(j + k)
         else:
@@ -35,26 +40,31 @@ def add(stk):
         stk.append(k)
 
 def subt(stk):
-    if len(stk) == 0: return
+    if len(stk) == 0:
+        stk.append(input())
     k = stk.pop()
     if type(k) is list:
         k = [i for i in k if (i and i != ' ')]
         stk.append(k)
     elif type(k) in (int, long, float):
+        j = 0
         if len(stk) == 0:
-            stk.append(k)
-            return
-        j = stk.pop()
+            j = input()
+            j,k = k,j
+        else:
+            j = stk.pop()
         if type(j) in (int, long, float):
             stk.append(j - k)
         else:
             stk.append(j)
             stk.append(k)
     elif type(k) is str:
+        j = 0
         if len(stk) == 0:
-            stk.append(k)
-            return
-        j = stk.pop()
+            j = input()
+            j,k = k,j
+        else:
+            j = stk.pop()
         if type(j) is str:
             stk.append(j.replace(k, ''))
         else:
@@ -64,7 +74,8 @@ def subt(stk):
         stk.append(k)
 
 def mult(stk):
-    if len(stk) == 0: return
+    if len(stk) == 0:
+        stk.append(input())
     k = stk.pop()
     if type(k) is list:
         if all(isinstance(i, (int, long, float)) for i in k):
@@ -72,10 +83,12 @@ def mult(stk):
         else:
             stk.append(k)
     elif type(k) in (int, long, float):
+        j = 0
         if len(stk) == 0:
-            stk.append(k)
-            return
-        j = stk.pop()
+            j = input()
+            j,k = k,j
+        else:
+            j = stk.pop()
         if type(j) in (int, long, float):
             stk.append(j * k)
         elif type(k) in (int, long) and type(j) is str:
@@ -84,10 +97,12 @@ def mult(stk):
             stk.append(j)
             stk.append(k)
     elif type(k) is str:
+        j = 0
         if len(stk) == 0:
-            stk.append(k)
-            return
-        j = stk.pop()
+            j = input()
+            j,k = k,j
+        else:
+            j = stk.pop()
         if type(j) in (int, long):
             stk.append(j * k)
         else:
@@ -97,13 +112,16 @@ def mult(stk):
         stk.append(k)
 
 def divd(stk):
-    if len(stk) == 0: return
+    if len(stk) == 0:
+        stk.append(input())
     k = stk.pop()
     if type(k) in (int, long, float):
+        j = 0
         if len(stk) == 0:
-            stk.append(k)
-            return
-        j = stk.pop()
+            j = input()
+            j,k = k,j
+        else:
+            j = stk.pop()
         if type(j) in (int, long, float):
             stk.append(float(j) / float(k))
         else:
@@ -113,13 +131,16 @@ def divd(stk):
         stk.append(k)
 
 def divint(stk):
-    if len(stk) == 0: return
+    if len(stk) == 0:
+        stk.append(input())
     k = stk.pop()
     if type(k) in (int, long, float):
+        j = 0
         if len(stk) == 0:
-            stk.append(k)
-            return
-        j = stk.pop()
+            j = input()
+            j,k = k,j
+        else:
+            j = stk.pop()
         if type(j) in (int, long, float):
             stk.append(int(j / k))
         else:
@@ -135,7 +156,8 @@ def wrap(stk):
     stk.append(newl)
 
 def uwrap(stk):
-    if len(stk) == 0: return
+    if len(stk) == 0:
+        stk.append(input())
     k = stk.pop()
     if type(k) is list:
         for item in k:
@@ -152,14 +174,16 @@ def rinp(stk):
     except: return
 
 def sort(stk):
-    if len(stk) == 0: return
+    if len(stk) == 0:
+        stk.append(input())
     k = stk.pop()
     if type(k) in (str, list): stk.append(sorted(k))
     elif type(k) in (int, long, float): stk.append(-1 * k)
     else: stk.append(k)
 
 def fact(stk):
-    if len(stk) == 0: return
+    if len(stk) == 0:
+        stk.append(input())
     k = stk.pop()
     if type(k) in (int, long, float):
         if type(k) is float: k = math.gamma(k+1)
@@ -188,15 +212,18 @@ def swap(stk):
     stk.append(j)
 
 def minm(stk):
-    if len(stk) == 0: return
+    if len(stk) == 0:
+        stk.append(input())
     k = stk.pop()
     if type(k) is list:
         stk.append(min(k))
     else:
+        j = 0
         if len(stk) == 0:
-            stk.append(k)
-            return
-        j = stk.pop()
+            j = input()
+            j,k = k,j
+        else:
+            j = stk.pop()
         if type(j) is not list:
             stk.append(j < k)
         else:
@@ -204,15 +231,18 @@ def minm(stk):
             stk.append(k)
 
 def maxm(stk):
-    if len(stk) == 0: return
+    if len(stk) == 0:
+        stk.append(input())
     k = stk.pop()
     if type(k) is list:
         stk.append(max(k))
     else:
+        j = 0
         if len(stk) == 0:
-            stk.append(k)
-            return
-        j = stk.pop()
+            j = input()
+            j,k = k,j
+        else:
+            j = stk.pop()
         if type(j) is not list:
             stk.append(j > k)
         else:
@@ -220,7 +250,8 @@ def maxm(stk):
             stk.append(k)
 
 def dupl(stk):
-    if len(stk) == 0: return
+    if len(stk) == 0:
+        stk.append(input())
     k = stk.pop()
     stk.append(k)
     stk.append(k)
